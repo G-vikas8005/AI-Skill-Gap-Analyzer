aecf7f82b9bba8241491f1a8525753137fb9058e
G-vikas8055 <Govindalavikas63@gmail.com>
2026-06-19 16:22:14 +0530

    Fix CORS configuration by correcting brackets

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(
   cors({
     origin:[ "http://localhost:5173",
              "https://ai-skill-gap-analyzer-frontend.onrender.com",
-            ]
+            ],
     credentials: true,
   })
 );
