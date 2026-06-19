ea8d076af5fda2b591a3d7809b4a04a437d62a3f
G-vikas8055 <Govindalavikas63@gmail.com>
2026-06-19 16:28:51 +0530

    Update API base URL to include '/api' path

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
+// const API = axios.create({
+
+//   baseURL: "https://ai-skill-gap-analyzer-backend.onrender.com",
+
+// });
 const API = axios.create({
-
-  baseURL: "https://ai-skill-gap-analyzer-backend.onrender.com",
-
+  baseURL: "https://ai-skill-gap-analyzer-backend.onrender.com/api",
 });
 
 export default API;
